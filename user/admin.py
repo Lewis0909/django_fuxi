9dc5c114fc3472e5ef1910633b26c6c1bab57b42
小东 <13121335440@163.com>
2021-04-20 15:24:39 +0800

    复习git第一次将暂存区的代码提交到仓库区

---
+++ user/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+# 导入模型
+from user.models import BookInfo,PeopleInfo
+
+# Register your models here.
+
+# 注册书籍模型
+admin.site.register(BookInfo)
+# 注册人物模型
+admin.site.register(PeopleInfo)
---
+++ user/urls.py
@@ -0,0 +1,39 @@
+from django.conf.urls import url
+from user.views import index,bookList,bookinfo,bookpage,views,param,keys,jsonresponse,response,CenterView,BookView
+
+app_name='user'
+
+# 固定写法
+urlpatterns = [
+    url(r'^$',index),
+
+    # 书籍页面url配置
+    url(r'^booklist/$',bookList),
+
+    # 数据库操作
+    url(r'^bookinfo/$',bookinfo),
+
+    # 分页
+    url(r'^bookpage/$',bookpage),
+
+    #　视图
+    url(r'^views/$',views),
+
+    # 位置参数
+    url(r'^param/(\d+)/(\d+)/$',param),
+
+    # 关键字参数
+    url(r'^(?P<name>\w+)/(?P<sex>\w+)/$',keys),
+
+    # json响应
+    url(r'^json/$',jsonresponse,name='test'),
+
+    # 重定向
+    url(r'^redirect/$',response),
+
+
+    url(r'^login/$',BookView.as_view()),
+
+    # 类视图函数url
+    url(r'^center/$',CenterView.as_view())
+]
